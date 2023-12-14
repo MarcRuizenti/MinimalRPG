c69ec3d34a0698a0ad88a9ce7ec9e26ec65b8961
Domenicano777 <146344487+Domenicano777@users.noreply.github.com>
2023-12-14 09:13:03 +0100

    player struct

---
+++ MinimalRPG/Player.h
@@ -0,0 +1,19 @@
+#pragma once
+#include<iostream>
+#include<cstdlib>
+#include<time.h>
+#include<string>
+#include<vector>
+
+struct Player
+{
+	int potitionX;
+	int potitionY;
+	int gold;
+	int health;
+	int healthMax;
+	int stamina;
+	int staminaMAx;
+	int agility;
+	int potion;
+};
---
+++ MinimalRPG/main.cpp
@@ -0,0 +1,20 @@
+
+enum Scene {DUNGEON, COMBAT, CHEST, GAMEOVER};
+
+int main() {
+	Scene currentScene = DUNGEON;
+
+	switch (currentScene)
+	{
+	case DUNGEON:
+		break;
+	case COMBAT:
+		break;
+	case CHEST:
+		break;
+	case GAMEOVER:
+		break;
+	default:
+		break;
+	}
+}
