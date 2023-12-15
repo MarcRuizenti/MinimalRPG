570c1cef91397a026b0db3bf70473012003b18dc
MarcRuizenti <146344001+MarcRuizenti@users.noreply.github.com>
2023-12-15 13:45:25 +0100

    mostrar mapa

--- MinimalRPG/MostarMapa.cpp
+++ MinimalRPG/MostarMapa.cpp
@@ -1,5 +1,18 @@
 #include "MostrarMapa.h"
 
-void MostrarMapa(vector<vector<char>>* map, int sizeX, int sizeY) {
+void MostrarMapa(vector<vector<char>> map, int sizeX, int sizeY) {
+	cout << " ___  ___  ___  ___  ___" << endl;
+	for (int i = 0; i < sizeY; i++) {
+		for (int j = 0; j < sizeX; j++) {
+			cout << "|   ||   ||   ||   ||   |" << endl;
 
+			if (j != sizeX - 1) {
+				cout << "| " << map[i][j] << " |";
+			}
+			else {
+				cout << "| " << map[i][j] << " |" << endl;
+			}
+			cout << "|___||___||___||___||___|" << endl;
+		}
+	}
 }
