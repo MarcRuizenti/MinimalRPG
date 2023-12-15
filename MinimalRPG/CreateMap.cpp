8d100d5a440fe41ac329d739f4ec5534389741d8
MarcRuizenti <146344001+MarcRuizenti@users.noreply.github.com>
2023-12-15 13:23:14 +0100

    no funciona con punteros Xd

--- MinimalRPG/CreateMap.cpp
+++ MinimalRPG/CreateMap.cpp
@@ -1,23 +1,23 @@
 #include "CreateMap.h"
 
-vector<vector<char>>* CreateMap(MainManager* mm, int sizeX, int sizeY) {
-	vector<vector<char>>* temp = new vector<vector<char>>;
-	temp->resize(sizeX, vector<char>(sizeY));
+vector<vector<char>> CreateMap(MainManager* mm, int sizeX, int sizeY) {
+	vector<vector<char>> temp;
+	temp.resize(sizeX, vector<char>(sizeY));
 
 	for (int i = 0; i < sizeY; i++) {
 		for (int j = 0; i < sizeX; j++) {
-			temp->at(i).at(j) = ' ';
+			temp[i][j] = ' ';
 			if (i == mm->p->position.Y && j == mm->p->position.X) {
-				temp->at(i).at(j) = 'P';
+				temp[i][j] = 'P';
 			}
 			for (int k = 0; k < mm->c.size(); k++) {
 				if (i == mm->c[k]->position.Y && j == mm->c[k]->position.X) {
-					temp->at(i).at(j) = 'C';
+					temp[i][j] = 'C';
 				}
 			}
 			for (int k = 0; k < mm->enemies.size(); k++) {
 				if (i == mm->enemies[k]->position.Y && j == mm->enemies[k]->position.X && !mm->enemies[k]->isDead) {
-					temp->at(i).at(j) = 'E';
+					temp[i][j] = 'E';
 				}
 			}
 
