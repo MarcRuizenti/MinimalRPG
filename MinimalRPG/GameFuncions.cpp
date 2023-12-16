1cc0ea1879f7200dfc7f48b01ff462f645220a04
Marc Ruiz Garcia <marc.ruiz.garcia@enti.cat>
2023-12-17 00:46:28 +0100

    interfaz ahora si

--- MinimalRPG/GameFuncions.cpp
+++ MinimalRPG/GameFuncions.cpp
@@ -6,22 +6,37 @@ void Dungeon(MainManager* mm) {
 
 	int sixeX = 5;
 	int sixeY = 5;
-
+	string input;
 
 	cout << "------ DUNGEON ------" << endl << endl;
 	
 	cout << "E -> Enemy  P -> Player  C -> Chest" << endl << endl;
 
 	cout << "Health: " << mm->p->health << " / " << mm->p->maxHealth << endl;
-	cout << "Potions: " << mm->p->position << " / " << mm->p->maxPotion << endl;
+	cout << "Postions: " << mm->p->potion << " / " << mm->p->maxPotion << endl;
 	cout << "Moves: " << mm->p->agility << " / " << mm->p->maxAgility << endl << endl;
 
-	
 	MostrarMapa(CreateMap(mm, sixeX, sixeY), sixeX, sixeY);
 
+	cout << "_______________________" << endl << endl;
+
 	cout << "W A S D -> Move" << endl;
-	cout << "p -> Postions" << endl;
+	cout << "p -> Postions" << endl << endl;
 	
+
+	cout << "Enter your action: ";
+	cin >> input;
+
+
+	if (input == "W" || input == "A" || input == "S" || input == "D") {
+
+	}
+	else if (input == "P") {
+
+	}
+	else {
+		cout << "Icorect input" << endl << endl;
+	}
 
 	system("pause");
 }
