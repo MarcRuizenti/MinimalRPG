8345496baf7be111f72d82c7dce221355bc0c9d1
Marc Ruiz Garcia <marc.ruiz.garcia@enti.cat>
2023-12-17 00:33:34 +0100

    interfaz dungeon

--- MinimalRPG/GameFuncions.cpp
+++ MinimalRPG/GameFuncions.cpp
@@ -2,11 +2,26 @@
 #include "MostrarMapa.h"
 
 void Dungeon(MainManager* mm) {
-	
+	system("cls");
 
 	int sixeX = 5;
 	int sixeY = 5;
+
+
+	cout << "------ DUNGEON ------" << endl << endl;
+	
+	cout << "E -> Enemy  P -> Player  C -> Chest" << endl << endl;
+
+	cout << "Health: " << mm->p->health << " / " << mm->p->maxHealth << endl;
+	cout << "Potions: " << mm->p->position << " / " << mm->p->maxPotion << endl;
+	cout << "Moves: " << mm->p->agility << " / " << mm->p->maxAgility << endl << endl;
+
+	
 	MostrarMapa(CreateMap(mm, sixeX, sixeY), sixeX, sixeY);
+
+	cout << "W A S D -> Move" << endl;
+	cout << "p -> Postions" << endl;
+	
 
 	system("pause");
 }
