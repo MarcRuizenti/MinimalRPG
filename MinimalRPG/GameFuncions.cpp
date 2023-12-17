5b70665c2f054172e640edb1e477fa8c39c682c7
Marc Ruiz Garcia <marc.ruiz.garcia@enti.cat>
2023-12-17 10:46:47 +0100

    case sentitive y move

--- MinimalRPG/GameFuncions.cpp
+++ MinimalRPG/GameFuncions.cpp
@@ -1,11 +1,12 @@
 #include "GameFunctions.h"
 #include "MostrarMapa.h"
+#include "Move.h"
 
 void Dungeon(MainManager* mm) {
 	system("cls");
 
-	int sixeX = 5;
-	int sixeY = 5;
+	int sizeX = 5;
+	int sizeY = 5;
 	string input;
 
 	cout << "------ DUNGEON ------" << endl << endl;
@@ -16,7 +17,7 @@ void Dungeon(MainManager* mm) {
 	cout << "Postions: " << mm->p->potion << " / " << mm->p->maxPotion << endl;
 	cout << "Moves: " << mm->p->agility << " / " << mm->p->maxAgility << endl << endl;
 
-	MostrarMapa(CreateMap(mm, sixeX, sixeY), sixeX, sixeY);
+	MostrarMapa(CreateMap(mm, sizeX, sizeY), sizeX, sizeY);
 
 	cout << "_______________________" << endl << endl;
 
@@ -27,9 +28,14 @@ void Dungeon(MainManager* mm) {
 	cout << "Enter your action: ";
 	cin >> input;
 
+	for (int i = 0; i < input.size(); i++) {
+		if (input[i] >= 'a' && input[i] <= 'z') {
+			input[i] -= ' ';
+		}
+	}
 
 	if (input == "W" || input == "A" || input == "S" || input == "D") {
-
+		Move(mm, input, sizeX, sizeY);
 	}
 	else if (input == "P") {
 
--- MinimalRPG/Move.cpp
+++ MinimalRPG/Move.cpp
@@ -2,21 +2,37 @@
 
 void Move(MainManager* mm, string input, int sizeX, int sizeY) {
 	if (input == "W") {
-		if (mm->p->position.X - 1 < 0) {
+		if (mm->p->position.Y - 1 < 0) {
 			cout << "Te salres del mapa" << endl;
 			return;
 		}
 		else {
-			mm->p->position.X -= 1;
+			mm->p->position.Y -= 1;
 		}
 	}
 	else if (input == "A") {
-
+		if (mm->p->position.X - 1 < 0) {
+			cout << "Te salres del mapa" << endl;
+		}
+		else{
+			mm->p->position.X -= 1;
+		}
 	}
 	else if (input == "S"){
-
+		if (mm->p->position.Y + 1 > sizeY) {
+			cout << "Te salres del mapa" << endl;
+		}
+		else {
+			mm->p->position.Y += 1;
+		}
 	}
 	else if (input == "D") {
+		if (mm->p->position.X + 1 > sizeX) {
+			cout << "Te salres del mapa" << endl;
 
+		}
+		else {
+			mm->p->position.X += 1;
+		}
 	}
 }
