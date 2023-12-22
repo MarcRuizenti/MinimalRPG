ec18300d0c0f62196cfaa9e57a2ca813990fbc29
Sergi Domenech Molina <sergi.domenech@enti.cat>
2023-12-22 20:03:44 +0100

    couts en el combat logic

--- MinimalRPG/CombatLogic.cpp
+++ MinimalRPG/CombatLogic.cpp
@@ -27,23 +27,26 @@ void CombatLogic(MainManager* mm, string input, int enemy) {
 				mm->enemies[enemy]->health -= attackPlayerStamina;
 				mm->enemies[enemy]->stamina -= enemyAttackStamina;
 				mm->p->stamina -= attackPlayerStamina;
+				cout << "You strike with more force! the enemy receives" << attackPlayerStamina << " damage" << endl;
 			}
 			else {
 				mm->enemies[enemy]->stamina -= enemyAttackStamina;
 				mm->p->health -= enemyAttackStamina;
 				mm->p->stamina -= attackPlayerStamina;
+				cout << "The enemy strikes with more force! you receive " << enemyAttackStamina << " damage" << endl;
 			}
 		}
 		else if (enemyatack == 'R') {
 			mm->p->stamina -= attackPlayerStamina;
 			mm->enemies[enemy]->health == mm->enemies[enemy]->healthMax;
 			mm->enemies[enemy]->health -= attackPlayerStamina;
-
+			cout << "You strike the unprepared enemy dealing " << attackPlayerStamina << " damage" << endl;
 		}
 		else if (enemyatack == 'D') {
 			mm->enemies[enemy]->health -= attackPlayerStamina - ((attackPlayerStamina * 75) / 100);
 			mm->enemies[enemy]->stamina += (mm->enemies[enemy]->staminaMax * 25) / 100;
 			mm->p->stamina -= attackPlayerStamina;
+			cout << "You strike the enemy but he has defended himself, the enemy receive " << attackPlayerStamina - ((attackPlayerStamina * 75) / 100) << " damage" << endl;
 		}
 	}
 	else if (input == "D") {
@@ -52,14 +55,17 @@ void CombatLogic(MainManager* mm, string input, int enemy) {
 			mm->enemies[enemy]->stamina -= enemyAttackStamina;
 			mm->p->health -= enemyAttackStamina - ((enemyAttackStamina * 75) / 100);
 			mm->p->health += (mm->p->maxStamina * 25) / 100;
+			cout << "You defend the enemy blow, but receive " << enemyAttackStamina - ((enemyAttackStamina * 75) / 100) << " damage" << endl;
 		}
 		else if (enemyatack == 'R') {
 			mm->enemies[enemy]->health == mm->enemies[enemy]->healthMax;
 			mm->p->stamina += (mm->p->maxStamina * 25) / 100;
+			cout << "You defend while the enemy catches a breath! It seems ready to strike!" << endl;
 		}
 		else if (enemyatack == 'D') {
 			mm->enemies[enemy]->stamina += (mm->enemies[enemy]->staminaMax * 25) / 100;
 			mm->p->stamina += (mm->p->maxStamina * 25) / 100;
+			cout << "You both defend, it seems like it will be another metapod fight" << endl;
 		}
 	}
 	else if (input == "R") {
@@ -68,14 +74,17 @@ void CombatLogic(MainManager* mm, string input, int enemy) {
 			mm->enemies[enemy]->stamina -= enemyAttackStamina;
 			mm->p->health -= enemyAttackStamina;
 			mm->p->health == mm->p->maxHealth;
+			cout << "You rest when the enemy hits you, striking for " << enemyAttackStamina << "damage" << endl;
 		}
 		else if (enemyatack == 'R') {
 			mm->enemies[enemy]->health == mm->enemies[enemy]->healthMax;
 			mm->p->health == mm->p->maxHealth;
+			cout << "You both rest, gathering up for a clash!" << endl;
 		}
 		else if (enemyatack == 'D') {
 			mm->enemies[enemy]->stamina += (mm->enemies[enemy]->staminaMax * 25) / 100;
 			mm->p->health == mm->p->maxHealth;
+			cout << "You rest when the enemy defend, 300 IQ play!" << endl;
 		}
 	}
 }
