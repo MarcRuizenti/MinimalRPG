175ede12e05079f5863f96a9145fdcc2bda95225
Marc Ruiz Garcia <marc.ruiz.garcia@enti.cat>
2024-01-01 20:48:14 +0100

    Mejoar mapa

--- MinimalRPG/CreateMap.cpp
+++ MinimalRPG/CreateMap.cpp
@@ -2,26 +2,21 @@
 
 vector<vector<char>> CreateMap(MainManager* mm, int sizeX, int sizeY) {
 	vector<vector<char>> temp;
-	temp.resize(sizeX, vector<char>(sizeY));
+	temp.resize(sizeY, vector<char>(sizeX));
 
-	for (int i = 0; i < sizeY; i++) {
-		for (int j = 0; j < sizeX; j++) {
-			temp[i][j] = ' ';
-			if (i == mm->p->position.Y && j == mm->p->position.X) {
-				temp[i][j] = 'P';
-			}
-			for (int k = 0; k < mm->c.size(); k++) {
-				if (i == mm->c[k]->position.Y && j == mm->c[k]->position.X && !mm->c[k]->isLooted) {
-					temp[i][j] = 'C';
-				}
-			}
-			for (int k = 0; k < mm->enemies.size(); k++) {
-				if (i == mm->enemies[k]->position.Y && j == mm->enemies[k]->position.X && !mm->enemies[k]->isDead) {
-					temp[i][j] = 'E';
-				}
-			}
+	temp[mm->p->position.Y][mm->p->position.X] = 'P';
 
-		}
+	for (int i = 0; i < mm->c.size(); i++) {
+		if (!mm->c[i]->isLooted) 
+			temp[mm->c[i]->position.Y][mm->c[i]->position.X] = 'C';
+		
 	}
+			
+	for (int i = 0; i < mm->enemies.size(); i++) {
+		if (!mm->enemies[i]->isDead)
+			temp[mm->enemies[i]->position.Y][mm->enemies[i]->position.X] = 'C';
+
+	}
+	
 	return temp;
 }
