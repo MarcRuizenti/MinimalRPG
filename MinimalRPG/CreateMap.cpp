cd05ba305d50a1cac0b55729914a9dee0518d813
Marc Ruiz Garcia <marc.ruiz.garcia@enti.cat>
2024-01-01 21:05:35 +0100

    Mejor mejor mapa

--- MinimalRPG/CreateMap.cpp
+++ MinimalRPG/CreateMap.cpp
@@ -4,6 +4,12 @@ vector<vector<char>> CreateMap(MainManager* mm, int sizeX, int sizeY) {
 	vector<vector<char>> temp;
 	temp.resize(sizeY, vector<char>(sizeX));
 
+	for (int i = 0; i < temp.size(); i++) {
+		for (int j = 0; j < temp.size(); j++) {
+			temp[i][j] = ' ';
+		}
+	}
+
 	temp[mm->p->position.Y][mm->p->position.X] = 'P';
 
 	for (int i = 0; i < mm->c.size(); i++) {
@@ -14,7 +20,7 @@ vector<vector<char>> CreateMap(MainManager* mm, int sizeX, int sizeY) {
 			
 	for (int i = 0; i < mm->enemies.size(); i++) {
 		if (!mm->enemies[i]->isDead)
-			temp[mm->enemies[i]->position.Y][mm->enemies[i]->position.X] = 'C';
+			temp[mm->enemies[i]->position.Y][mm->enemies[i]->position.X] = 'E';
 
 	}
 	
