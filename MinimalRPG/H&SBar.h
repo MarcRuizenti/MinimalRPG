dc79f5e2bbac1e829c73936e9af1785cd0a81728
MarcRuizenti <marc.ruiz.garcia@enti.cat>
2024-01-14 19:07:58 +0100

    H&Sbar cpp

--- MinimalRPG/H&SBar.h
+++ MinimalRPG/H&SBar.h
@@ -1,26 +1,6 @@
 #pragma once
 #include "MapPosition.h"
 
-void ProgressBarHealth(int current, int max) {
-	const int barWidth = 10;
-	int barLength = barWidth * current / max;
+void ProgressBarHealth(int current, int max);
 
-	std::cout << "[";
-	for (int i = 0; i < barWidth; ++i) {
-		std::cout << (i < barLength ? "=" : " ");
-	}
-	std::cout << "] ";
-
-}
-
-void ProgressBarStamina(int current, int max) {
-	const int barWidth = 10;
-	int barLength = barWidth * current / max;
-
-	std::cout << "[";
-	for (int i = 0; i < barWidth; ++i) {
-		std::cout << (i < barLength ? ">" : " ");
-	}
-	std::cout << "] ";
-
-}
+void ProgressBarStamina(int current, int max);
